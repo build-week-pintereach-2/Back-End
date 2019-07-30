bb0b84fd4f7c9c55843a35176b754e48e6da9545
micahjones13 <micahjones13@gmail.com>
2019-07-30 13:22:53 -0600

    updated seeds for user_pins to be ints

--- data/seeds/003-user_pins.js
+++ data/seeds/003-user_pins.js
@@ -5,11 +5,11 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('user_pins').insert([
-        { user_id: '2', pin_id: '6'},
-        { user_id: '3', pin_id: '4'},
-        { user_id: '2', pin_id: '1'},
-        { user_id: '1', pin_id: '2'},
-        { user_id: '2', pin_id: '3'}
+        { user_id: 2, pin_id: 6},
+        { user_id: 3, pin_id: 4},
+        { user_id: 2, pin_id: 1},
+        { user_id: 1, pin_id: 2},
+        { user_id: 2, pin_id: 3}
       ]);
     });
 };
